ef90a102f6fad3fbab6913b77c1b4bd7ba3b9bc5
Niyetkhan <nietxan.s@gmail.com>
2024-12-21 23:26:28 +0500

    feat: basic file w/ args, append, write

--- src/ed.c
+++ src/ed.c
@@ -1,6 +1,59 @@
+#include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 
-int main(void)
+int fcount(FILE *f)
 {
-	printf("Hello World!\n");
+	char c;
+	int count;
+
+	while (1) {
+		c = fgetc(f);
+		if (c == EOF)
+			break;
+		++count;
+	}		
+
+	return count;
+}
+
+FILE *f;
+char *fname;
+char fbuf[2048];
+
+int main(int argc, char **argv)
+{
+	if (argc > 1) {
+		fname = argv[1];
+		f = fopen(fname, "w+");
+		if (f == NULL) {
+			fprintf(stderr, "file error\n");
+			return 1;
+		}
+		printf("%d\n", fcount(f));
+	}
+
+	char input;
+	char buf[1024];
+
+	while (1) {
+		scanf("%c", &input);
+		switch (input) {
+			case 'a':
+				while (1) {
+					scanf("%s", buf);
+					if (!strcmp(buf, ".")) {
+						break;
+					}
+					strcat(fbuf, buf);
+				}
+				break;
+			case 'w':
+				printf("%d\n", fprintf(f, "%s\n", fbuf));
+				break;
+			case 'q':
+				fclose(f);
+				return 0;
+		}
+	}
 }
