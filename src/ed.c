87c00a14b021878e7cb125d8b09447f0b243fdd5
Niyetkhan <nietxan.s@gmail.com>
2024-12-30 17:13:05 +0500

    feat: command processing

--- src/ed.c
+++ src/ed.c
@@ -1,41 +1,72 @@
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+
+#define INPUT_SIZE 1024
+
+void strtrim(char *s)
+{
+	char * p = s;
+	int l = strlen(p);
+
+	while(isspace(p[l - 1])) p[--l] = 0;
+	while(*p && isspace(*p)) ++p, --l;
+
+	memmove(s, p, l + 1);
+}
+
+int tokcount(char *input, char tok)
+{
+	int count = 0;
+	char *ptr = input;
+	while ((ptr = strchr(ptr, tok)) != NULL) {
+		count++;
+		ptr++;
+	}
+	return count;
+}
 
 /* Main loop with command processing */	
 
 int ed(FILE *f)
 {
-	// TODO: put in for (;;) {}
-	char input[1024];
-	
-	fgets(input, 1024, stdin);
-
+	char *temp;
+	char *input;
 	char *cmd[2];
 
-	int count = 0;
-	char *ptr = input;
-	while((ptr = strchr(ptr, ' ')) != NULL) {
-		count++;
-		ptr++;
-	}
-
-	if (count != 1)
+	for (;;) {
+		input = (char*)malloc(INPUT_SIZE * sizeof(char));
 		cmd[0] = "?";
-	else {
-		for (int i = 0; i < 2; ++i)
-			cmd[i] = strtok(input, " ");
+		cmd[1] = "";
+
+		fgets(input, INPUT_SIZE, stdin);
+		if (strcmp(input, "\n")) {
+			strtrim(input);
+
+			switch (tokcount(input, ' ')) {
+				case 0:
+					if (strlen(input) == 1)
+						cmd[0] = input;
+				case 1:
+					temp = strtok(input, " ");
+					/* TODO: more advanced cmd processing */
+					if (strlen(temp) == 1) {
+						cmd[0] = temp;
+						cmd[1] = strtok(NULL, " ");
+					}
+			}
+		}
+
+		switch ((int)(*cmd[0])) {
+			case 'q':
+				if (cmd[1] == NULL) {
+					return 0;
+				}
+			default:
+				printf("?\n");
+				free(input);
+		}
 	}
-	
-	if (strlen(cmd[0]) != 1)
-		cmd[0] = "?";
-
-	switch ((char)(*cmd[0])) {
-		// TODO: cases		
-		case '?':
-			printf("?\n");
-			break;
-	}
-
-
-	return 0;
 }
